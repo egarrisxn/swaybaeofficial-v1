f10dad6870e9fd8d2371a6779b6da5d6e19b2d2c
egarrisxn <egarrisxn@gmail.com>
2025-12-02 18:45:43 -0500

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,27 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
-import prettier from "eslint-config-prettier";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+/** @type {import('eslint').Linter.Config[]} */
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next/core-web-vitals", "prettier"],
     rules: {
       "no-unused-vars": "off",
       // "no-use-before-define": "off",
       // "no-undef": "off",
     },
+  }),
+  {
+    ignores: ["**/node_modules/", ".git/", ".vscode/", ".next/", "public/"],
   },
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
-  prettier,
-]);
+];
 
 export default eslintConfig;
